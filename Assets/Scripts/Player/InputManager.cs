5f36536949f92ff9f946ba82fdabc8418ca499e2
brappo <benjamin.rappo69@gmail.com>
2025-02-07 21:29:24 +0100

    set the inputManager to be a singleton

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -1,25 +1,34 @@
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    PlayerController    _playerController;
-    private void    Awake()
-    {
-        _playerController = new PlayerController();
+	private static InputManager _instance;
 
-        _playerController.World.Enable();
-        _playerController.World.rotateWorld.performed += Rotate;
-    }
-    
-    private void    OnDestroy()
-    {
-        _playerController.World.Disable();
-    }
+	private PlayerController    _playerController;
 
-    private void    Rotate(InputAction.CallbackContext context)
-    {
-        Debug.Log(context.ReadValue<float>());
-    }
+	private void    Start()
+	{
+		if (_instance != null)
+		{
+			Debug.LogError("Multiples instances of the Inputmanager class !");
+			Destroy(this);
+			return ;
+		}
+		_playerController = new PlayerController();
+
+		_playerController.World.Enable();
+		_playerController.World.rotateWorld.performed += Rotate;
+		_instance = this;
+	}
+
+	private void    OnDestroy()
+	{
+		_playerController.World.Disable();
+	}
+
+	private void    Rotate(InputAction.CallbackContext context)
+	{
+		Debug.Log(context.ReadValue<float>());
+	}
 }
