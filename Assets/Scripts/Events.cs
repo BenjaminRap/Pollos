d3d0a86aa0708affcdc40058acaa8a4eff4d4848
brappo <benjamin.rappo69@gmail.com>
2025-02-08 11:50:33 +0100

    added the smoke effects on fall

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -2,13 +2,45 @@ using UnityEngine;
 
 public class Events : MonoBehaviour
 {
+	private static Events	_instance;
+
+	[SerializeField]
+	private Animator		_smokeEffect;
+	
+	private void	Start()
+	{
+		if (_instance != null)
+		{
+			Debug.LogError("Multiples instances of the Events class !");
+			Destroy(this);
+			return ;
+		}
+		_instance = this;
+	}
+
 	public static void	spawnFallingBoxShockEffect(GameObject gameObject)
 	{
-		Debug.Log("The falling box has been shocked !");
+		if (_instance == null)
+		{
+			Debug.LogError("There is no instance of the Events class but an eventt function has been called !");
+			return;
+		}
+		Animator	effect = Instantiate(_instance._smokeEffect , gameObject.transform.position,
+				gameObject.transform.rotation);
+		float effectDuration = effect.GetCurrentAnimatorStateInfo(0).length;
+		Destroy(effect.gameObject, effectDuration);
 	}
 	
 	public static void	spawnPollosShockEffect(GameObject gameObject)
 	{
-		Debug.Log("The pollos has been shocked !");
+		if (_instance == null)
+		{
+			Debug.LogError("There is no instance of the Events class but an eventt function has been called !");
+			return;
+		}
+		Animator	effect = Instantiate(_instance._smokeEffect , gameObject.transform.position,
+				gameObject.transform.rotation);
+		float effectDuration = effect.GetCurrentAnimatorStateInfo(0).length;
+		Destroy(effect.gameObject, effectDuration);
 	}
 }
