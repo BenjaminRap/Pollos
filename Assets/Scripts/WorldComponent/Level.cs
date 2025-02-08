97ebd4ff4f886e964f5a34ac243b52ad3803b63f
brappo <benjamin.rappo69@gmail.com>
2025-02-08 20:57:35 +0100

    the other rigidbodyes now behave the same as the pollos

--- Assets/Scripts/WorldComponent/Level.cs
+++ Assets/Scripts/WorldComponent/Level.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
 	private const float		_rotationAngle = 90.0f;
+	private const float		_velocityMultiplicatorAtRotation = 0.4f;
 	
 	[SerializeField]
 	private float			_rotationDuration = 0.2f;
@@ -20,6 +22,7 @@ public class Level : MonoBehaviour
 	private Quaternion		_rotationGoal;
 	private Storm[]			_storms;
 	private float			_averageStormDistanceAtStart;
+	private Rigidbody2D[]	_rigidBodys;
 
 	private void		Start()
 	{
@@ -33,6 +36,7 @@ public class Level : MonoBehaviour
 		_rotationGoal = _rotableChild.transform.rotation;
 		_storms = GetComponentsInChildren<Storm>();
 		_averageStormDistanceAtStart = GetAverageStormDistance();
+		_rigidBodys = GetComponentsInChildren<Rigidbody2D>();
 	}
 	
 	private void	Update()
@@ -62,7 +66,31 @@ public class Level : MonoBehaviour
 	private IEnumerator	RotateLevelToRotationGoal(CharacterControler characterController)
 	{
 		yield return StartCoroutine(TransformUtils.RotateInTime(_rotableChild.transform, _rotationGoal, _rotationDuration));
-		characterController.RestartSimulation();
+		StartRigidbodysSimulation();
+	}
+	
+	private  void	StopRigidbodysSimulationInGrid()
+	{
+		foreach (Rigidbody2D rigidbody in _rigidBodys)
+		{
+			Vector3	newPosition;
+			Vector3	position = rigidbody.transform.position;
+
+			newPosition.x = MathF.Round(position.x + 0.5f) - 0.5f;
+			newPosition.y = MathF.Round(position.y + 0.5f) - 0.5f;
+			newPosition.z = position.z;
+			rigidbody.transform.position = newPosition;
+			rigidbody.linearVelocity *= _velocityMultiplicatorAtRotation;
+			rigidbody.simulated = false;
+		}
+	}
+
+	private void	StartRigidbodysSimulation()
+	{
+		foreach (Rigidbody2D rigidbody in _rigidBodys)
+		{
+			rigidbody.simulated = true;
+		}
 	}
 	
 	public void			Rotate(float axisValue)
@@ -81,7 +109,7 @@ public class Level : MonoBehaviour
 		}
 		if (_rotateCoroutine != null)
 			StopCoroutine(_rotateCoroutine);
-		characterControler.StopSimulation();
+		StopRigidbodysSimulationInGrid();
 		_rotationGoal *= Quaternion.Euler(-axisValue * _rotationAngle * Vector3.forward);
 		_rotateCoroutine = StartCoroutine(RotateLevelToRotationGoal(characterControler));
 	}
