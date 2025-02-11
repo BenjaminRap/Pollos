c4008e4ee341f32bd027746d9431264760c34b5e
brappo <benjamin.rappo69@gmail.com>
2025-02-11 19:54:16 +0100

    set to the norm

--- Assets/Scripts/Player/SetActivePop.cs
+++ Assets/Scripts/Player/SetActivePop.cs
@@ -2,20 +2,10 @@ using UnityEngine;
 
 public class SetActivePop : MonoBehaviour
 {
-    [SerializeField] private GameObject _fade = null;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[SerializeField] private GameObject _fade = null;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void PlayGame()
-    {
-        _fade.SetActive(true);
-    }
+	public void PlayGame()
+	{
+		_fade.SetActive(true);
+	}
 }
--- Assets/Scripts/Player/UIManager.cs
+++ Assets/Scripts/Player/UIManager.cs
@@ -1,62 +1,56 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject _playButton = null;
-    [SerializeField] private GameObject _quitButton = null;
-    [SerializeField] private GameObject _creditButton = null;
-    [SerializeField] private GameObject _uiCredit= null;
-    [SerializeField] private GameObject _cancelImage = null;
-    [SerializeField] private Animator _animatorFade= null;
-    [SerializeField] private Animator _animatorPlay= null;
+	[SerializeField] private GameObject _playButton = null;
+	[SerializeField] private GameObject _quitButton = null;
+	[SerializeField] private GameObject _creditButton = null;
+	[SerializeField] private GameObject _uiCredit= null;
+	[SerializeField] private GameObject _cancelImage = null;
+	[SerializeField] private Animator _animatorFade= null;
+	[SerializeField] private Animator _animatorPlay= null;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        DontDestroyOnLoad(this._animatorFade.transform.parent);
-    }
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		DontDestroyOnLoad(this._animatorFade.transform.parent);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void OnCreditClic()
-    {
-        _uiCredit.SetActive(true);
-        _playButton.SetActive(false);
-        _quitButton.SetActive(false);
-        _creditButton.SetActive(false);
-    }
-    public void OnExitCreditClic()
-    {
-        //_uiCredit.SetActive(false);
-        //_playButton.SetActive(true);
-        //_quitButton.SetActive(true);
-        //_creditButton.SetActive(true);
-    }
-    public IEnumerator PlayGameCoroutine()
-    {
-        _animatorPlay.SetBool("PlayClick",true);
-        _cancelImage.SetActive(true);
-        yield return new WaitForSeconds(8);
-        SceneManager.LoadSceneAsync("InGame");
-    }
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
-    public void OnClic()
-    {
-        if (AudioManager.TryAndGetInstance(out AudioManager audioManager))
-            audioManager.PlayAudioEffect("Clic", gameObject.transform.position, 2);
-    }
-    public void PlayGame()
-    {
-        StartCoroutine(PlayGameCoroutine());
-    }
+	public void OnCreditClic()
+	{
+		_uiCredit.SetActive(true);
+		_playButton.SetActive(false);
+		_quitButton.SetActive(false);
+		_creditButton.SetActive(false);
+	}
+	public void OnExitCreditClic()
+	{
+		//_uiCredit.SetActive(false);
+		//_playButton.SetActive(true);
+		//_quitButton.SetActive(true);
+		//_creditButton.SetActive(true);
+	}
+	public IEnumerator PlayGameCoroutine()
+	{
+		_animatorPlay.SetBool("PlayClick",true);
+		_cancelImage.SetActive(true);
+		yield return new WaitForSeconds(8);
+		SceneManager.LoadSceneAsync("InGame");
+	}
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+	public void OnClic()
+	{
+		if (AudioManager.TryAndGetInstance(out AudioManager audioManager))
+			audioManager.PlayAudioEffect("Clic", gameObject.transform.position, 2);
+	}
+	public void PlayGame()
+	{
+		StartCoroutine(PlayGameCoroutine());
+	}
 }
