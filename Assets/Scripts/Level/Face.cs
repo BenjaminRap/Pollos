5614a8a36bbd2ee6895d604d461146ec6b132afc
brappo <benjamin.rappo69@gmail.com>
2025-02-14 11:39:07 +0100

    added a new map and the Face script

---
+++ Assets/Scripts/Level/Face.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+	[SerializeField]
+	private Face	_upFace;
+	[SerializeField]
+	private Face	_downFace;
+	[SerializeField]
+	private Face	_rightFace;
+	[SerializeField]
+	private Face	_leftFace;
+}
