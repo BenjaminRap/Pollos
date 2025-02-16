ab547e92776328cb1bcfb5c4097a7a3be505fc92
brappo <benjamin.rappo69@gmail.com>
2025-02-16 11:20:06 +0100

    added the arrows to each faces

--- Assets/Scripts/Level/Cube.cs
+++ Assets/Scripts/Level/Cube.cs
@@ -29,23 +29,23 @@ public class Cube : MonoBehaviour
 	private Quaternion							_localRotation;
 	
 	private void Start()
-    {
+	{
 		_localRotation = Quaternion.identity;
 		// When getting the current visible face, we must return the face opposite
 		// to the frontFaceDirection vector. That is why we inverse the up and down vector.
 		// As we are applying the rotation on the vector.back, the rotation on the y axis
 		// are already inversed. That's just a guess though.
 		_faces = new()
-        {
-            { Vector3Int.down, _upFace },
-            { Vector3Int.up, _downFace },
-            { Vector3Int.right, _rightFace },
-            { Vector3Int.left, _leftFace },
-            { Vector3Int.forward, _forwardFace },
-            { Vector3Int.back, _backFace }
-        };
+		{
+			{ Vector3Int.down, _upFace },
+			{ Vector3Int.up, _downFace },
+			{ Vector3Int.right, _rightFace },
+			{ Vector3Int.left, _leftFace },
+			{ Vector3Int.forward, _forwardFace },
+			{ Vector3Int.back, _backFace }
+		};
 		ShowPossibleRotations(Quaternion.identity);
-    }
+	}
 	
 	public GameObject			GetFace(Quaternion _localRotation, Quaternion rotation)
 	{
@@ -63,9 +63,9 @@ public class Cube : MonoBehaviour
 	
 	public void	ShowPossibleRotations(Quaternion _localRotation)
 	{
-		_upArrow.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(-90, Vector3.right)) != null);
-		_downArraw.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(90, Vector3.right)) != null);
-		_rightArrow.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(-90, Vector3.up)) != null);
-		_leftArrow.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(90, Vector3.up)) != null);
+		// _upArrow.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(-90, Vector3.right)) != null);
+		// _downArraw.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(90, Vector3.right)) != null);
+		// _rightArrow.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(-90, Vector3.up)) != null);
+		// _leftArrow.enabled = (GetFace(_localRotation, Quaternion.AngleAxis(90, Vector3.up)) != null);
 	}
 }
---
+++ Assets/Scripts/Level/Face.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+	[SerializeField]
+	private SpriteRenderer						_upArrow;
+	[SerializeField]
+	private SpriteRenderer						_downArraw;
+	[SerializeField]
+	private SpriteRenderer						_rightArrow;
+	[SerializeField]
+	private SpriteRenderer						_leftArrow;
+
+    private void Start()
+    {
+        Debug.Log(transform.rotation.eulerAngles);
+    }
+}
