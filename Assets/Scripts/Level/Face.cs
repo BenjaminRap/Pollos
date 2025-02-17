87e88a7acb6a4ba125a4c478930e98b7afba7fb6
brappo <benjamin.rappo69@gmail.com>
2025-02-17 22:06:07 +0100

    Updating the Rotatable exclude layer mask when they rotate from a face to another

--- Assets/Scripts/Level/Face.cs
+++ Assets/Scripts/Level/Face.cs
@@ -65,6 +65,8 @@ public class Face : MonoBehaviour
 	
 	public void	SetParentToRigidbody(Rigidbody rigidbody)
 	{
+		rigidbody.linearVelocity = Vector3.zero;
+		rigidbody.excludeLayers = ~(1 << gameObject.layer);
 		rigidbody.transform.SetParent(transform);
 		rigidbody.transform.localPosition = TransformUtils.SetZ(rigidbody.transform.localPosition, 0.0f);
 		rigidbody.transform.gameObject.layer = gameObject.layer;
